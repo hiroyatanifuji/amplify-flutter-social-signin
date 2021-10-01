372521085970dcbecb5147acd632da3e79a13d83
hiroya tanifuji <h.tanifuji@mono-tech.co.jp>
2021-10-01 18:19:07 +0900

    feat: implement MyApp widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/material.dart';
+
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify.dart';
-import 'package:flutter/material.dart';
+
+import 'package:amplify_flutter_social_signin/pages/loading_page.dart';
+import 'package:amplify_flutter_social_signin/pages/mypage.dart';
+import 'package:amplify_flutter_social_signin/pages/sign_in_page.dart';
 
 import 'amplifyconfiguration.dart';
 
@@ -14,7 +19,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  bool _isAmplifyConfigured = false;
+  bool _isInitialized = false;
+  AuthUser? _user;
 
   @override
   initState() {
@@ -24,33 +30,77 @@ class _MyAppState extends State<MyApp> {
 
   void _initAmplifyFlutter() async {
     AmplifyAuthCognito auth = AmplifyAuthCognito();
-
     Amplify.addPlugins([auth]);
 
     try {
       await Amplify.configure(amplifyconfig);
-    } on AmplifyAlreadyConfiguredException {
-      print("Amplify was already configured. Looks like app restarted on android.");
+
+      final user = await Amplify.Auth.getCurrentUser();
+
+      setState(() {
+        _user = user;
+      });
+    } on AmplifyAlreadyConfiguredException catch (e) {
+      print(e.message);
+    } on SignedOutException catch (e) {
+      print(e.message);
     }
 
     setState(() {
-      _isAmplifyConfigured = true;
+      _isInitialized = true;
     });
   }
 
   Widget _display() {
-    if (_isAmplifyConfigured) {
-      return Container();
+    if (!_isInitialized) {
+      return const LoadingPage();
     } else {
-      return Container();
+      return _user != null
+          ? MyPage(
+              username: _user!.username,
+              userId: _user!.userId,
+              signOut: _signOut,
+            )
+          : SignIn(
+              signIn: _signIn,
+            );
     }
   }
 
-  // This widget is the root of your application.
+  Future<void> _signOut() async {
+    try {
+      await Amplify.Auth.signOut();
+      setState(() {
+        _user = null;
+      });
+    } on AuthException catch (e) {
+      print(e.message);
+    }
+  }
+
+  Future<void> _signIn() async {
+    try {
+      final res = await Amplify.Auth.signInWithWebUI();
+      if (res.isSignedIn) {
+        try {
+          final user = await Amplify.Auth.getCurrentUser();
+
+          setState(() {
+            _user = user;
+          });
+        } on SignedOutException catch (e) {
+          print(e.message);
+        }
+      }
+    } on AuthException catch (e) {
+      print(e.message);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Amplify App',
+      title: 'Amplify Flutter App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
